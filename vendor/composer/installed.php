61ebd237c467f990dff2030d4908275ea488b257
Abrahão Martins <abrahaosrmartins@gmail.com>
2022-06-05 19:17:30 -0300

    [Chore] Composer config

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '093f9efbcdfc84ce4740ee3d43a5050b56faa1e1',
+        'name' => 'abrsr/app_shopping_cart',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'abrsr/app_shopping_cart' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '093f9efbcdfc84ce4740ee3d43a5050b56faa1e1',
+            'dev_requirement' => false,
+        ),
+    ),
+);
